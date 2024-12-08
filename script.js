f06243bb68751601acc9a696dbb1bc9a58f0cfca
4rved <arvedunbehaun@gmx.de>
2024-12-08 16:06:31 +0100

    new init

---
+++ script.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById('field');
+const ctx = canvas.getContext('2d');
+
+const players = [
+  { x: 100, y: 200, color: 'blue', dx: 2 },
+  { x: 700, y: 200, color: 'red', dx: -2 },
+];
+
+function drawPlayers() {
+  players.forEach(player => {
+    ctx.beginPath();
+    ctx.arc(player.x, player.y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = player.color;
+    ctx.fill();
+    ctx.closePath();
+  });
+}
+
+function updatePositions() {
+  players.forEach(player => {
+    player.x += player.dx;
+    if (player.x < 10 || player.x > canvas.width - 10) {
+      player.dx *= -1;
+    }
+  });
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPlayers();
+  updatePositions();
+  requestAnimationFrame(gameLoop);
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(() => {
+      console.log('Service Worker registered!');
+    });
+  }
+  
+gameLoop();
